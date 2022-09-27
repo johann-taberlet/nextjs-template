59790211514250d254a1f59273f0213c4d896f18
Johann <johann.taberlet@gmail.com>
2022-09-27 15:48:24 +0200

    fix: jest config to improve coverage

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,12 @@
-module.exports = {
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+});
+
+const customJestConfig = {
   rootDir: process.cwd(),
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   testPathIgnorePatterns: [
@@ -7,6 +15,11 @@ module.exports = {
     '<rootDir>/coverage',
     '<rootDir>/dist',
     '<rootDir>/e2e/',
+  ],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/utils/report-accessibility.ts',
+    '!src/pages/_app.tsx',
   ],
   testEnvironment: 'jsdom',
   transform: {
@@ -24,3 +37,5 @@ module.exports = {
     ],
   },
 };
+
+module.exports = createJestConfig(customJestConfig);
