6dbeb157faa0c85d0ba35bbcb4fb2e1a38282697
Johann <johann.taberlet@gmail.com>
2022-09-27 11:41:53 +0200

    chore(dockerfile): added Dockerfile to the project

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  output: 'standalone',
   i18n: {
     locales: ['fr-FR', 'en-GB'],
     defaultLocale: 'fr-FR',
